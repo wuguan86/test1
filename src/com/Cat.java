338bb033da5cd9fb9718ba5aa79b7965a505a2b9
wuguan <591004761@qq.com>
2017-04-24 11:27:23 +0800

    sda

--- src/com/Cat.java
+++ src/com/Cat.java
@@ -1,10 +1,17 @@
 package com;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Cat {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 	System.out.println("you  are cat");
 	System.out.println("ss");
+	FileOutputStream  fos=new  FileOutputStream("xx.txt");
 	
 	
+	fos.write("97".getBytes());
+	fos.close();
 }
 }
